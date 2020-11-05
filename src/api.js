ea5b733c54eade8155e549e3dfc519afcfc24a25
AndyJhoao <andy_joao@live.com>
2020-11-04 19:16:08 -0600

    Create Fetch of coincap and implement in the table

---
+++ src/api.js
@@ -0,0 +1,44 @@
+const url = "https://api.coincap.io/v2";
+
+function getAssets() {
+  return getRequest("assets?limit=20");
+}
+
+function getRequest(method) {
+  return fetch(`${url}/${method}`)
+    .then((response) => response.json())
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      console.log("Error", error);
+    });
+}
+
+export default {
+  getAssets,
+};
+// const url = "https://api.coincap.io/v2";
+
+// function getAssets() {
+//   return getRequest("assets?limit=20");
+// }
+
+// function getRequest(method) {
+//   return fetch(`${url}/${method}`, {
+//     mode: "cors",
+//     method: "GET",
+//     headers: {
+//       Accept: "application/json",
+//     },
+//   })
+//     .then((response) => response.json())
+//     .then((response) => {
+//       return response.data;
+//     })
+//     .catch((error) => {
+//       console.log("request failed", error);
+//     }); // Syntax error: unexpected end of input
+// }
+
+// export default { getAssets };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex-col items-center">
-    <px-assets-table />
+    <px-assets-table :assets="assets" />
     <router-link to="/about" class="hover:underline flex from-purple-900 mt-10"
       >About</router-link
     >
@@ -8,11 +8,20 @@
 </template>
 
 <script>
+import api from "@/api";
 import pxAssetsTable from "@/components/pxAssetsTable";
 export default {
   name: "Home",
   components: {
     pxAssetsTable,
+  },
+  data() {
+    return {
+      assets: [],
+    };
+  },
+  created() {
+    api.getAssets().then((assets) => (this.assets = assets));
   },
 };
 </script>
